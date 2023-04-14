27f264c72b9c48ad16636a4b9735ef77172f8f20
Daniel Jakob <github@usox.org>
2023-04-14 11:18:43 +0200

    Update composer targets
    Also add rector to qa tests

--- rector.php
+++ rector.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\Config\RectorConfig;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Php80\Rector\Catch_\RemoveUnusedVariableInCatchRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Set\ValueObject\SetList;
@@ -19,10 +19,12 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(resource: SetList::CODE_QUALITY);
     $rectorConfig->import(SetList::DEAD_CODE);
     $rectorConfig->import(SetList::PHP_81);
-    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
 
     $services = $rectorConfig->services();
-    $services->set(TypedPropertyRector::class);
     $services->set(ClassPropertyAssignToConstructorPromotionRector::class);
     $services->set(RemoveUnusedVariableInCatchRector::class);
+
+    $rectorConfig->skip([
+        FlipTypeControlToUseExclusiveTypeRector::class,
+    ]);
 };
